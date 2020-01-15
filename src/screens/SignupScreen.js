e253ee520d9229757984f477ac6ade2346cd616a
Wagner Paz <wagner.o.paz@gmail.com>
2020-01-14 21:38:34 -0300

    Made the intial interface for the Sign Up screen.

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,16 +1,53 @@
-import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {Text, Input, Button} from 'react-native-elements';
+
+import Spacer from '../components/Spacer';
+import SigninScreen from './SigninScreen';
 
 const SignupScreen = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <View>
-            <Text>SignupScreen</Text>
-            <Button title="Go to Sign In" onPress={() => navigation.navigate('Signin')}/>
-            <Button title="Go to Main Flow" onPress={() => navigation.navigate('mainFlow')}/>
+        <View style={styles.container}>
+            <Spacer><Text h3>Sign Up for Tracker</Text></Spacer>
+            <Spacer>
+                <Input
+                    label="Email"
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
+            </Spacer>
+            <Spacer>
+                <Input
+                    label="Password"
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    secureTextEntry
+                />
+            </Spacer>
+            <Spacer><Button title="Sign Up"/></Spacer>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+    return {
+        header: null,
+    };
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 80,
+    }
+});
 
 export default SignupScreen;
